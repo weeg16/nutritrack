cedbe3ed674c42a4912474d36e6d5f97c641e4c9
luis miguel <luis_robles@dlsu.edu.ph>
2025-07-16 00:32:12 +0800

    remote url test

--- app/src/main/java/com/mobicom/s18/nutritrack_mco/MainActivity.java
+++ app/src/main/java/com/mobicom/s18/nutritrack_mco/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //
 
         Button loginBtn = findViewById(R.id.loginBtn);
         TextView registerBtn = findViewById(R.id.registerBtn);
